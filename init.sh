6b3c4d251882d7f6bc13c8dc685503fb68a30256
obamoose <obamoose@gmail.com>
2021-02-13 13:12:13 +0100

    Revert "Update init.sh"
    This reverts commit 53fd6684192b420587bd53026e63ac8572542733.

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 cat mod.list | grep -e '^\w' | sed -e 's/^[[:space:]]*(.*)[[:space:]]*$/$1/' | while read -r repo; do
-git clone https://github.com/purrplingcat/$repo.git
+   git clone git@github.com:purrplingcat/$repo.git
 done
 
